7403c848b3cf465f7fef501129ba4ac195d72a07
sw.han <sw.han@wego-robotics.com>
2025-10-12 01:00:58 +0900

    for test

--- 6__train.sh
+++ 6__train.sh
@@ -1,11 +1,21 @@
-lerobot-train   \
-    --policy.device=cuda  \
-    --policy.type=smolvla   \
-    --dataset.repo_id=wego-hansu/piper_pick_yellow_car   \
-    --dataset.video_backend=pyav   \
-    --batch_size=64   \
-    --steps=20000   \
-    --eval_freq=5000   \
-    --output_dir=outputs/train/piper_smolvla_pick_yellow_cars_new   \
-    --job_name=piper_smolvla_yellow_car   \
-    --wandb.enable=false
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate lerobot
+
+HF_USER=$(hf auth whoami | head -n 1)
+echo $HF_USER
+
+python ./src/lerobot/scripts/train.py \
+--policy.device=cuda  \
+--policy.type=smolvla   \
+--policy.repo_id=piper_smolvla  \
+--policy.push_to_hub=false \
+--dataset.repo_id=wego-hansu/piper_pick_yellow_car_033   \
+--dataset.video_backend=pyav   \
+--batch_size=64   \
+--steps=25000   \
+--eval_freq=0   \
+--output_dir=outputs/train/piper_smolvla_pick_yellow_cars_033   \
+--job_name=piper_smolvla_yellow_car   \
+--wandb.enable=false  \
+--resume=true  \
+--config_path=outputs/train/piper_smolvla_pick_yellow_cars_033/checkpoints/020000/pretrained_model/train_config.json\
--- 8__run_client.sh
+++ 8__run_client.sh
@@ -6,7 +6,7 @@ echo $HF_USER
     # --task="Grasp the object and put it in the bin" \
 
 python src/lerobot/scripts/server/robot_client.py \
-    --server_address=127.0.0.1:8080 \
+    --server_address=192.168.0.42:8088 \
     --robot.type=piper_follower \
     --robot.port=can0 \
     --robot.id=black \
@@ -14,9 +14,10 @@ python src/lerobot/scripts/server/robot_client.py \
         top: {type: opencv, index_or_path: 0, width: 640, height: 480, fps: 30}, \
         left: {type: opencv, index_or_path: 4, width: 640, height: 480, fps: 30}}" \
     --policy_type=smolvla \
-    --pretrained_name_or_path=outputs/train/piper_smolvla_pick_yellow_cars/checkpoints/020000/pretrained_model \
+    --pretrained_name_or_path=wego-hansu/piper_smolvla \
     --policy_device=cuda \
-    --actions_per_chunk=50  \
+    --actions_per_chunk=5  \
     --chunk_size_threshold=0.5 \
     --aggregate_fn_name=weighted_average \
-    --debug_visualize_queue_size=True
+    --debug_visualize_queue_size=True \
+    --task="Grab the yellow car and put in the box"  \
